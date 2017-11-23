3ae36a167686e8d7009f6eac5ce79c031d40a065
Tobias Martin <tobias.martin@sc5.io>
2017-11-23 17:43:06 +0200

    styles

--- src/components/Teaser/Teaser.js
+++ src/components/Teaser/Teaser.js
@@ -1,31 +1,32 @@
 import React from "react";
 
 import styles from "./Teaser.css";
+import typo from "../Typography/Typography.css";
 
 const Teaser = () => {
   return (
     <div className={styles.teaser}>
-      <div className="date-container">
-        <div className="teaser-date">
-          <span className="day">17</span>
-          <span className="month-year">
+      <div className={styles.dateContainer}>
+        <div className={styles.teaserDate}>
+          <span className={styles.day}>17</span>
+          <span className={styles.monthYear}>
             Nov
             <br />
             2017
           </span>
         </div>
       </div>
-      <div className="teaser-content">
+      <div className={styles.teaserContent}>
         <h2>Teaser heading</h2>
-        <div className="author">Author name</div>
-        <div className="teaser-text">
+        <div className={styles.author}>Author name</div>
+        <div className={styles.teaserText}>
           <p />
         </div>
-        <a href="#" className="button">
+        <a href="#" className={styles.button}>
           Read more
         </a>
       </div>
-      <div className="teaser-tags">
+      <div className={styles.teaserTags}>
         <ul>
           <li>
             <a href="#">Teaser tag</a>
